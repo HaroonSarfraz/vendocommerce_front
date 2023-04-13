1b6c7da60c5aeee64f223b8f6861ba758d6ee11f
Haroon Sarfraz <haroon@remotereps.com>
2023-04-13 20:32:02 +0500

    table added on total-revenue page

--- pages/advertising-analytics/total-revenue-acos/index.js
+++ pages/advertising-analytics/total-revenue-acos/index.js
@@ -2,16 +2,169 @@ import { useState } from "react";
 import dynamic from "next/dynamic";
 import TopBarFilter from "@/src/components/advertising-analytics/top-bar-filter";
 import Graph from "@/src/components/advertising-analytics/total-revenue/Graph";
+import Loading from "@/src/components/loading";
+import ASINTable from "@/src/components/table";
 
 const DashboardLayout = dynamic(() => import("@/src/layouts/DashboardLayout"), {
   ssr: false,
 });
 
 export default function Users() {
+  const [loading, setLoading] = useState(false);
+
   const [filter, setFilter] = useState({
     week: [],
     year: 2023,
   });
+
+  const list = [];
+
+  const columns = [
+    {
+      title: "WEEK",
+      width: "80px",
+      align: "center",
+      // render: (text) => {
+      //   return <span>{text?.week_name}</span>;
+      // },
+    },
+    {
+      title: "SPEND",
+      width: "80px",
+      align: "center",
+      // render: (text) => {
+      //   return <span>{text?.week_name}</span>;
+      // },
+    },
+    {
+      title: "SPEND CHG",
+      width: "120px",
+      align: "center",
+      // render: (text) => {
+      //   return <span>{text?.week_name}</span>;
+      // },
+    },
+    {
+      title: "AD REVENUE",
+      width: "130px",
+      align: "center",
+      // render: (text) => {
+      //   return <span>{text?.week_name}</span>;
+      // },
+    },
+    {
+      title: "AD CHG",
+      width: "90px",
+      align: "center",
+      // render: (text) => {
+      //   return <span>{text?.week_name}</span>;
+      // },
+    },
+    {
+      title: "ORGANIC SALES",
+      width: "150px",
+      align: "center",
+      // render: (text) => {
+      //   return <span>{text?.week_name}</span>;
+      // },
+    },
+    {
+      title: "ORGANIC CHG",
+      width: "140px",
+      align: "center",
+      // render: (text) => {
+      //   return <span>{text?.week_name}</span>;
+      // },
+    },
+    {
+      title: "TOTAL SALES",
+      width: "130px",
+      align: "center",
+      // render: (text) => {
+      //   return <span>{text?.week_name}</span>;
+      // },
+    },
+    {
+      title: "TOTAL ACOS",
+      width: "120px",
+      align: "center",
+      // render: (text) => {
+      //   return <span>{text?.week_name}</span>;
+      // },
+    },
+    {
+      title: "IMPRESSIONS",
+      width: "130px",
+      align: "center",
+      // render: (text) => {
+      //   return <span>{text?.week_name}</span>;
+      // },
+    },
+    {
+      title: "CLICKS",
+      width: "90px",
+      align: "center",
+      // render: (text) => {
+      //   return <span>{text?.week_name}</span>;
+      // },
+    },
+    {
+      title: "TOTAL UNIT ORDERS",
+      width: "180px",
+      align: "center",
+      // render: (text) => {
+      //   return <span>{text?.week_name}</span>;
+      // },
+    },
+    {
+      title: "BRANDED SPEND",
+      width: "160px",
+      align: "center",
+      // render: (text) => {
+      //   return <span>{text?.week_name}</span>;
+      // },
+    },
+    {
+      title: "BRANDED SALES",
+      width: "160px",
+      align: "center",
+      // render: (text) => {
+      //   return <span>{text?.week_name}</span>;
+      // },
+    },
+    {
+      title: "BRANDED ROAS",
+      width: "160px",
+      align: "center",
+      // render: (text) => {
+      //   return <span>{text?.week_name}</span>;
+      // },
+    },
+    {
+      title: "NON BRANDED SPEND",
+      width: "200px",
+      align: "center",
+      // render: (text) => {
+      //   return <span>{text?.week_name}</span>;
+      // },
+    },
+    {
+      title: "NON BRANDED SALES",
+      width: "200px",
+      align: "center",
+      // render: (text) => {
+      //   return <span>{text?.week_name}</span>;
+      // },
+    },
+    {
+      title: "NON BRANDED ROAS",
+      width: "200px",
+      align: "center",
+      // render: (text) => {
+      //   return <span>{text?.week_name}</span>;
+      // },
+    },
+  ];
 
   return (
     <DashboardLayout>
@@ -20,6 +173,31 @@ export default function Users() {
           {TopBarFilter(filter, setFilter, "Week")}
 
           <Graph loading={false} chartData={true} />
+
+          <div className="mt-5 col-lg-12">
+            <div className="card mb-7 pt-5">
+              <div className="card-body pt-2">
+                {loading ? (
+                  <Loading />
+                ) : (
+                  <ASINTable
+                    columns={columns}
+                    dataSource={list}
+                    ellipsis
+                    rowKey="key"
+                    loading={loading}
+                    pagination={false}
+                    scroll={{
+                      x:
+                        columns
+                          ?.map((d) => d.width)
+                          .reduce((a, b) => a + b, 0) + 300,
+                    }}
+                  />
+                )}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </DashboardLayout>
