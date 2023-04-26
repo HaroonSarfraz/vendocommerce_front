203cdcd587c36c3d96ef22e60adb4ca5d5899ccd
Haroon Sarfraz <haroon@remotereps.com>
2023-04-27 00:24:42 +0500

    Fixed brands dropdown loading issue

--- pages/users/edit.js
+++ pages/users/edit.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
+import Loading from "@/src/components/loading";
 import { useRouter } from "next/router";
 import { useDispatch, useSelector } from "react-redux";
 import { Button, Checkbox, Form, Input, message, Select } from "antd";
@@ -30,6 +31,7 @@ export default function Users() {
   const [submit, setSubmit] = useState(false);
   const [permissions, setPermissions] = useState([]);
   const [brands, setBrands] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const brandList = useSelector((state) => state.brands.brandList);
 
@@ -37,20 +39,18 @@ export default function Users() {
 
   useEffect(() => {
     dispatch(getBrandList());
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
     fetchUserBrands(router?.query?.id)
       .then((res) => {
-        let brands_ = _.cloneDeep(brands);
-        brands_ = res?.data?.Brands.map((brand) => {
+        let brands_ = res?.data?.Brands.map((brand) => {
           return brand.id;
         });
         setBrands(brands_);
+        setLoading(false);
       })
       .catch((err) => message.error(err));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const onFinish = (values) => {
@@ -91,10 +91,6 @@ export default function Users() {
     return { label: user.u_amazon_seller_name, value: user.id };
   });
 
-  const handleChange = (value) => {
-    console.log(`selected ${value}`);
-  };
-
   const handleCheck = (item) => {
     const permissions_ = _.cloneDeep(permissions);
     const index = permissions.findIndex((perm) => perm === item);
@@ -128,160 +124,163 @@ export default function Users() {
                     <Icons type="user-lg" />
                     <h4 className="mx-5 mt-1">Personal Information</h4>
                   </div>
-                  <Form
-                    {...formItemLayout}
-                    layout="vertical"
-                    form={form}
-                    name="register"
-                    onFinish={onFinish}
-                  >
-                    <div className="row">
-                      <div className="col-12 col-sm-4 col-md-4 col-lg-4">
-                        <Form.Item
-                          name="u_name"
-                          label="Name"
-                          className="fw-bolder"
-                          rules={[
-                            {
-                              required: true,
-                              message: "Name is required",
-                            },
-                          ]}
-                          initialValue={user?.u_name ?? ""}
-                          hasFeedback
-                        >
-                          <Input size="large" autoFocus autoComplete="off" />
-                        </Form.Item>
+                  {loading || brandList.length < 1 ? (
+                    <Loading />
+                  ) : (
+                    <Form
+                      {...formItemLayout}
+                      layout="vertical"
+                      form={form}
+                      name="register"
+                      onFinish={onFinish}
+                    >
+                      <div className="row">
+                        <div className="col-12 col-sm-4 col-md-4 col-lg-4">
+                          <Form.Item
+                            name="u_name"
+                            label="Name"
+                            className="fw-bolder"
+                            rules={[
+                              {
+                                required: true,
+                                message: "Name is required",
+                              },
+                            ]}
+                            initialValue={user?.u_name ?? ""}
+                            hasFeedback
+                          >
+                            <Input size="large" autoFocus autoComplete="off" />
+                          </Form.Item>
+                        </div>
+                        <div className="col-12 col-sm-4 col-md-4 col-lg-4">
+                          <Form.Item
+                            name="u_email"
+                            label="E-mail"
+                            className="fw-bolder"
+                            rules={[
+                              {
+                                type: "email",
+                                message: "The input is not valid E-mail!",
+                              },
+                              {
+                                required: true,
+                                message: "E-mail is required",
+                              },
+                            ]}
+                            initialValue={user?.u_email ?? ""}
+                            hasFeedback
+                          >
+                            <Input size="large" autoComplete="off" />
+                          </Form.Item>
+                        </div>
+                        <div className="col-12 col-sm-4 col-md-4 col-lg-4">
+                          <Form.Item
+                            name="brand_ids"
+                            label="Brand Account(s)"
+                            className="fw-bolder"
+                            rules={[
+                              {
+                                required: brands.length > 0 ? false : true,
+                                message: "Brand Account(s) cannot be blank",
+                              },
+                            ]}
+                            hasFeedback
+                          >
+                            <Select
+                              mode="multiple"
+                              allowClear
+                              style={{
+                                width: "100%",
+                              }}
+                              size="large"
+                              placeholder="Select Brand Account(s) ..."
+                              options={options}
+                              defaultValue={brands}
+                            />
+                          </Form.Item>
+                        </div>
                       </div>
-                      <div className="col-12 col-sm-4 col-md-4 col-lg-4">
-                        <Form.Item
-                          name="u_email"
-                          label="E-mail"
-                          className="fw-bolder"
-                          rules={[
-                            {
-                              type: "email",
-                              message: "The input is not valid E-mail!",
-                            },
-                            {
-                              required: true,
-                              message: "E-mail is required",
-                            },
-                          ]}
-                          initialValue={user?.u_email ?? ""}
-                          hasFeedback
-                        >
-                          <Input size="large" autoComplete="off" />
-                        </Form.Item>
-                      </div>
-                      <div className="col-12 col-sm-4 col-md-4 col-lg-4">
-                        <Form.Item
-                          name="brand_ids"
-                          label="Brand Account(s)"
-                          className="fw-bolder"
-                          rules={[
-                            {
-                              required: brands.length > 0 ? false : true,
-                              message: "Brand Account(s) cannot be blank",
-                            },
-                          ]}
-                          hasFeedback
-                        >
-                          <Select
-                            mode="multiple"
-                            allowClear
-                            style={{
-                              width: "100%",
-                            }}
-                            size="large"
-                            placeholder="Select Brand Account(s) ..."
-                            options={options}
-                            defaultValue={brands}
-                            onChange={handleChange}
-                          />
-                        </Form.Item>
-                      </div>
-                    </div>
 
-                    <div className="row mb-3">
-                      <div className="col-12">
-                        <div className="border rounded">
-                          <div className="h-50px px-5 d-flex justify-content-between align-items-center">
-                            <div>
-                              <h6 className="py-1 mx-1 mb-0 font-medium-2">
-                                <Icons type="lock" />
-                                <span className="mx-2 align-middle">
-                                  Permission
-                                </span>
-                              </h6>
+                      <div className="row mb-3">
+                        <div className="col-12">
+                          <div className="border rounded">
+                            <div className="h-50px px-5 d-flex justify-content-between align-items-center">
+                              <div>
+                                <h6 className="py-1 mx-1 mb-0 font-medium-2">
+                                  <Icons type="lock" />
+                                  <span className="mx-2 align-middle">
+                                    Permission
+                                  </span>
+                                </h6>
+                              </div>
+                              <div className="custom-control custom-checkbox mr-2">
+                                <Checkbox
+                                  id="checkAll"
+                                  className="mx-2"
+                                  checked={_.isEqual(
+                                    permissions.sort(),
+                                    permissionFields.sort()
+                                  )}
+                                  onChange={handleCheckAll}
+                                />
+                                <label className="fw-bold" htmlFor="checkAll">
+                                  Select All
+                                </label>
+                              </div>
                             </div>
-                            <div className="custom-control custom-checkbox mr-2">
-                              <Checkbox
-                                id="checkAll"
-                                className="mx-2"
-                                checked={_.isEqual(
-                                  permissions.sort(),
-                                  permissionFields.sort()
-                                )}
-                                onChange={handleCheckAll}
-                              />
-                              <label className="fw-bold" htmlFor="checkAll">
-                                Select All
-                              </label>
-                            </div>
-                          </div>
 
-                          <div className="border-top px-2 pt-2">
-                            <div className="py-2 row">
-                              {permissionFields.map((item, index) => (
-                                <div
-                                  key={index}
-                                  className="my-2 col-lg-3 col-md-6"
-                                >
-                                  <div className="custom-control custom-checkbox mb-2">
-                                    <Checkbox
-                                      id={item}
-                                      className="mx-2"
-                                      onChange={() => handleCheck(item)}
-                                      checked={permissions.includes(item)}
-                                    />
-                                    <label className="fw-bold" htmlFor={item}>
-                                      {item}
-                                    </label>
+                            <div className="border-top px-2 pt-2">
+                              <div className="py-2 row">
+                                {permissionFields.map((item, index) => (
+                                  <div
+                                    key={index}
+                                    className="my-2 col-lg-3 col-md-6"
+                                  >
+                                    <div className="custom-control custom-checkbox mb-2">
+                                      <Checkbox
+                                        id={item}
+                                        className="mx-2"
+                                        onChange={() => handleCheck(item)}
+                                        checked={permissions.includes(item)}
+                                      />
+                                      <label className="fw-bold" htmlFor={item}>
+                                        {item}
+                                      </label>
+                                    </div>
                                   </div>
-                                </div>
-                              ))}
+                                ))}
+                              </div>
                             </div>
                           </div>
                         </div>
                       </div>
-                    </div>
 
-                    <div className="mt-8 pt-8 d-flex border-top">
-                      <Form.Item className="d-flex">
-                        <Button
-                          htmlType="submit"
-                          disabled={submit}
-                          className="btn btn-sm btn-primary"
-                        >
-                          {submit ? (
-                            <span>
-                              Please wait...
-                              <span className="spinner-border spinner-border-sm align-middle ms-2" />
-                            </span>
-                          ) : (
-                            <span className="indicator-label">Submit</span>
-                          )}
-                        </Button>
-                      </Form.Item>
+                      <div className="mt-8 pt-8 d-flex border-top">
+                        <Form.Item className="d-flex">
+                          <Button
+                            htmlType="submit"
+                            disabled={submit}
+                            className="btn btn-sm btn-primary"
+                          >
+                            {submit ? (
+                              <span>
+                                Please wait...
+                                <span className="spinner-border spinner-border-sm align-middle ms-2" />
+                              </span>
+                            ) : (
+                              <span className="indicator-label">Submit</span>
+                            )}
+                          </Button>
+                        </Form.Item>
 
-                      <Form.Item className="mx-5">
-                        <Button className="btn btn-sm btn-secondary">
-                          <span className="indicator-label">Reset</span>
-                        </Button>
-                      </Form.Item>
-                    </div>
-                  </Form>
+                        <Form.Item className="mx-5">
+                          <Button className="btn btn-sm btn-secondary">
+                            <span className="indicator-label">Reset</span>
+                          </Button>
+                        </Form.Item>
+                      </div>
+                    </Form>
+                  )}
                 </div>
               </div>
             </div>
