4f0bdabb4d84c8a0160b44c0924fac90e9c0ad9f
Noraiz123 <gnoraiz512@gmail.com>
2023-05-10 01:41:37 +0500

    reload issue fixed

--- src/api/request.js
+++ src/api/request.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { isClient } from "../helpers/isClient";
+import { deleteCookie } from "cookies-next";
+import { cookies } from "../constants/cookies";
 
 const request = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
@@ -31,8 +33,9 @@ request.interceptors.response.use(
   },
   (error) => {
     if (error?.response?.status === 401) {
-      isClient && window.location.assign("/login");
       isClient && localStorage.clear();
+      isClient && deleteCookie(cookies["TOKEN"]);
+      isClient && window.location.assign("/login");
     }
     return error.response;
   }
