fc8532abbda2b852aaf531843b3b2af80fc20b31
Haroon Sarfraz <haroon@remotereps.com>
2023-06-01 23:21:18 +0500

    Added SP credentials Tab

---
+++ src/components/brands/index.js
@@ -0,0 +1,9 @@
+import General from "./general-settings";
+import UserSettings from "./users-settings";
+import SPCredentials from "./sp-credentials";
+
+export {
+  General,
+  UserSettings,
+  SPCredentials,
+};
