3df9843f8b2adbcf390927897ca253b22c971d7c
Haroon Sarfraz <haroon@remotereps.com>
2023-06-10 00:33:13 +0500

    User Brands Modal & wait for ads auth callback

--- src/components/permissions/AccountsModal.js
+++ src/components/permissions/AccountsModal.js
@@ -1,16 +1,43 @@
+import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import ModalWrapper from "@/src/components/ModalWrapper";
-import Icons from "@/src/assets/icons";
 import { UserSvg } from "@/src/assets";
+import { fetchUserBrands } from "@/src/api/users.api";
+import NoData from "@/src/components/no-data";
+import { message } from "antd";
 
-export default function AccountsModal({ isOpen, closeModal, account }) {
+export default function AccountsModal({
+  isOpen,
+  closeModal,
+  account,
+  brandList,
+}) {
+  const [loading, setLoading] = useState(false);
+  const [brands, setBrands] = useState([]);
+
+  useEffect(() => {
+    console.log(account.id);
+    if (isOpen && account?.id) {
+      fetchUserBrands(account.id)
+        .then((res) => {
+          if (res.status >= 200 && res.status <= 299) {
+            const brandIds = res.data.brands.map((d) => d.brandId);
+            setBrands(brandList.data.filter((b) => brandIds.includes(b.id)));
+          } else {
+            setLoading(false);
+          }
+        })
+        .catch((err) => message.error(err?.response?.message));
+    }
+  }, [isOpen]);
+
   return (
     <ModalWrapper isOpen={isOpen} closeModal={closeModal}>
       <div className="w-md-500px max-h-500px">
         <div style={{ backgroundColor: "#F6F6F6" }} className="modal-header">
           <p className="modal-title fw-bolder fs-5">
-            {account}&apos;s Assigned Accounts
+            {account?.name}&apos;s Assigned Brands
           </p>
           <FontAwesomeIcon
             icon={faXmark}
@@ -19,22 +46,22 @@ export default function AccountsModal({ isOpen, closeModal, account }) {
           />
         </div>
         <div className="min-h-200px p-5">
-          <h5 className="badge badge-light-dark bg-light badge-pill mx-1 mb-2 fs-7 fw-bolder">
-            <UserSvg />
-            {"  "}BDirect Online
-          </h5>
-          <h5 className="badge badge-light-dark bg-light badge-pill mx-1 mb-2 fs-7 fw-bolder">
-            <UserSvg />
-            {"  "}High Ridge Brands
-          </h5>
-          <h5 className="badge badge-light-dark bg-light badge-pill mx-1 mb-2 fs-7 fw-bolder">
-            <UserSvg />
-            {"  "}VStora
-          </h5>
-          <h5 className="badge badge-light-dark bg-light badge-pill mx-1 mb-2 fs-7 fw-bolder">
-            <UserSvg />
-            {"  "}Schmidts galleria
-          </h5>
+          {loading ? (
+            <Loading />
+          ) : brands?.length != 0 ? (
+            <div>
+              {brands.map((brand) => {
+                return (
+                  <h5 key={brand.id} className="badge badge-light-dark bg-light badge-pill mx-1 mb-2 fs-7 fw-bolder">
+                    <UserSvg />{"  "}
+                    {brand.u_amazon_seller_name}
+                  </h5>
+                );
+              })}
+            </div>
+          ) : (
+            <NoData height={150} />
+          )}
         </div>
         <div className="modal-footer">
           <button
